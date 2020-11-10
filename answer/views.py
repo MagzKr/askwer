a298b8450d6740bddaefe055448a7ef9a3f3014c
magz <magzkr@gmail.com>
2020-11-10 16:06:16 +0300

    test
    (cherry picked from commit e22a96550d1a3eb6ebcbf0d79e07f65d774d7eb6)

--- answer/views.py
+++ answer/views.py
@@ -2,6 +2,14 @@ from .models import Answer
 from question.models import Question
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
+
+def text_valid(text):
+    if text != '':
+        return True
+    else:
+        return False
+
 
 def create_answer(request):
         new_answer = Answer()
@@ -11,8 +19,12 @@ def create_answer(request):
             new_answer.author = User.objects.get(username='Guest')
         new_answer.question = Question.objects.get(pk=request.POST.get('questionPk'))
         new_answer.text = request.POST.get('text')
-        new_answer.save()
-        return render(request, 'answer/single_answer.html', {'answer':new_answer})
+        if text_valid(new_answer.text):
+            new_answer.save()
+            return render(request, 'answer/single_answer.html', {'answer':new_answer})
+        else:
+            error = b'You give bad answer!'
+            return HttpResponseBadRequest(error)
 
 
 def get_answers(pk):
