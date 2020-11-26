79b39c1ea9637c9abc06a3ed45d2e00e701d0247
magz <magzkr@gmail.com>
2020-11-26 13:47:49 +0300

    travis fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ python manage.py makemigrations answer
 python manage.py makemigrations question
 python manage.py makemigrations account
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+gunicorn -b 0.0.0.0:8000 askwer.wsgi:application
