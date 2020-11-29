8f24a8f3e39f92aa51a694419763565bb55bd1b4
magz <magzkr@gmail.com>
2020-11-29 17:26:04 +0300

    heroku fix

--- askwer/settings.py
+++ askwer/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = bool(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", 'localhost 127.0.0.1').split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", 'localhost 127.0.0.1 askwer-app.herokuapp.com').split(" ")
 
 
 INSTALLED_APPS = [
