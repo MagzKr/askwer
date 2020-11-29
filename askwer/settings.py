203030f3038cb973c397edf49474f98f1337896d
magz <magzkr@gmail.com>
2020-11-29 17:28:26 +0300

    heroku fix

--- askwer/settings.py
+++ askwer/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,7 +70,8 @@ DATABASES = {
         "PORT": os.environ.get("SQL_PORT"),
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
