ed09d962cffd7c05d89cbc838011fd8c53fc1b32
magz <magzkr@gmail.com>
2020-12-03 21:47:12 +0300

    add tags and many fixes

--- askwer/urls.py
+++ askwer/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from question.views import question_list_view
-import debug_toolbar
 urlpatterns = [
     path('', question_list_view, name='question_list_view'),
     path('admin/', admin.site.urls),
